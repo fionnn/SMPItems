b53fc667488eae3afeeae6e81d0baf15d531ca70
squareGITHUB <44121785+squareGITHUB@users.noreply.github.com>
2020-05-14 22:49:21 +0100

    Update Hat.java

--- src/main/java/tfsmp/smpitems/item/Hat.java
+++ src/main/java/tfsmp/smpitems/item/Hat.java
@@ -8,7 +8,7 @@ public class Hat extends CustomItem
 {
     public Hat()
     {
-        super("Hat", Material.IRON_CHESTPLATE, Rarity.RARE, ItemType.ARMOR);
+        super("Hat", Material.IRON_HELMET, Rarity.RARE, ItemType.ARMOR);
         super.addLoreLine("&b+20% Speed");
         super.addLoreLine("&a+3 Health");
         super.addAttribute(Attribute.GENERIC_MAX_HEALTH, 23);
